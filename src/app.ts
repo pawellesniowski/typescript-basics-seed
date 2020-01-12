e503b2719a45f85516f8905c2695883dd839a49b
Pawel_Lesniowski <pawel_lesniowski@epam.com>
2020-01-12 15:08:07 +0100

    TypeScript types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log('Hello TypeScript!!!!');
+// function type:
+type sumOrderType = (pizzasNo: number, price?: number) => string;
+let sumOrder: sumOrderType;
+sumOrder = (pizzasNo, price = 10) => {
+  return `you total order is ${pizzasNo*price}`
+};
+
+// primitive type bigint, BigInt in JS is an object that can represent large numbers over 2^23 - 1
+
+// object type:
+type humanObjectType = {name: string, age: number, gender: string, children?: string[], getName: () => string}
+
+let pawel: humanObjectType;
+pawel = {name: 'pawel', age: 33, gender: "male", getName(){ return this.name}};
+
+console.log(pawel.getName());
+
+// array type:
+let sizes: string[];
+sizes = ['small', 'large', 'extra large'];
+// array generic / union type:
+type sizesType = Array< number | string >
+const sizes2:sizesType = [1, '2'];
+
+
